44e2aba7cf2d6f09bf950146d3a554a70bccd75b
Tristan Rice <rice@fn.lc>
2016-01-11 23:36:21 -0800

    Updated star rendering

--- build.js
+++ build.js
@@ -18531,7 +18531,7 @@ Polymer({
       });
       window.addEventListener('WebComponentsReady', function(e) {
         console.log('WebComponentsReady');
-        renderStars();
+        stars();
       });
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
