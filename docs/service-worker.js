48a4774cd0f22402ba65f0c7db3cfed689e50471
Tristan Rice <rice@fn.lc>
2017-03-11 22:29:47 -0800

    updates to day of site

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -29,7 +29,7 @@
 
 
 /* eslint-disable quotes, comma-spacing */
-var PrecacheConfig = [["/components/dayof-page/dayof-page.html","de8ffba4fc905218d4a81eceed280a6f"],["/components/index-page/index-page.html","ce950781551ef84a1ce4fd6efb56cb26"],["/components/main-app/main-app.html","7d7f2b9d0d9be2a0f59ed8032b25672a"],["/components/register-form/register-form.html","b494ee5df11408cb707ed453aaa0b74e"],["/components/rsvp-page/rsvp-page.html","b8725f4bf9b4589c85b6da8b07eb0575"],["/components/select-hackers/select-hackers.html","b33a846fb7bb6ef4c5f6850245fd1aa8"],["/components/sponsor-page/sponsor-page.html","e8f4e1ba45cba74a09862e761d2c1373"],["/components/stats-page/stats-page.html","1cf4af4dd8642dba1e895ccdd3155f5b"],["/index.html","16fb3355ebd30e99722aa6c48d84f384"]];
+var PrecacheConfig = [["/components/dayof-page/dayof-page.html","fa5aad0a7900a2dba7c6e324458047a0"],["/components/index-page/index-page.html","a75f981205d0516872bad364cf65d4b7"],["/components/main-app/main-app.html","abec9e7ceca3b3e2154773e275e51256"],["/components/register-form/register-form.html","b494ee5df11408cb707ed453aaa0b74e"],["/components/rsvp-page/rsvp-page.html","b8725f4bf9b4589c85b6da8b07eb0575"],["/components/select-hackers/select-hackers.html","b33a846fb7bb6ef4c5f6850245fd1aa8"],["/components/sponsor-page/sponsor-page.html","e8f4e1ba45cba74a09862e761d2c1373"],["/components/stats-page/stats-page.html","1cf4af4dd8642dba1e895ccdd3155f5b"],["/index.html","16fb3355ebd30e99722aa6c48d84f384"]];
 /* eslint-enable quotes, comma-spacing */
 var CacheNamePrefix = 'sw-precache-v1--' + (self.registration ? self.registration.scope : '') + '-';
 
